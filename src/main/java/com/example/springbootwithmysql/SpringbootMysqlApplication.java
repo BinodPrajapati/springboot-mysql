d9ea3ee5ed3e9ec8f6cbcd5791fbb09461accc43
8178076255 <srp.binod@gmail.com>
2022-09-20 16:20:45 +0530

    Test tessage

--- src/main/java/com/example/springbootwithmysql/SpringbootMysqlApplication.java
+++ src/main/java/com/example/springbootwithmysql/SpringbootMysqlApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootMysqlApplication {
 
+	// Entry class
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMysqlApplication.class, args);
 	}
